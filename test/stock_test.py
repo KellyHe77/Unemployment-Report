17f5b624b7d33a9eb8be2b664960f53cd52efe8d
KellyHe77 <jh2190@georgetown.edu>
2022-11-11 21:42:29 -0500

    test the stock file

--- test/stock_test.py
+++ test/stock_test.py
@@ -4,16 +4,12 @@ from app.stocks import fetch_stock_data, format_usd
 
 
 def test_usd_formatting():
-    # it should apply USD formatting:
     assert format_usd(4.50) == "$4.50"
 
-    # it should display two decimal places:
     assert format_usd(4.5) == "$4.50"
 
-    # it should round to two places:
     assert format_usd(4.55555) == "$4.56"
 
-    # it should display thousands separators:
     assert format_usd(1234567890.5555555) == "$1,234,567,890.56"
 
 
